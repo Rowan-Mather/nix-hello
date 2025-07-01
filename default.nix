3de9c5aa0579ad962d148f104557fe88e95da00b
Rowan Mather <76477727+Rowan-Mather@users.noreply.github.com>
2025-07-01 15:45:09 +0100

    Update default.nix

--- default.nix
+++ default.nix
@@ -3,18 +3,6 @@
 let
   pkgs = import nixpkgs {};
 in
-
-pkgs.stdenv.mkDerivation {
-  name = "hello-world";
-  version = "1.0";
-
-  unpackPhase = ":";
-  src = null;
-
-  buildPhase = ''
-    mkdir -p $out/bin
-    echo '#!/bin/sh' > $out/bin/hello-world
-    echo 'echo "Hello, World!"' >> $out/bin/hello-world
-    chmod +x $out/bin/hello-world
-  '';
+{
+  mytest = pkgs.runCommand "hi" {} "echo hi > $out";
 }
