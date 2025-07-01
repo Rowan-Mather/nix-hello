301c19ef8dcf9b837011482002b3f26e34ac84e2
Rowan Mather <76477727+Rowan-Mather@users.noreply.github.com>
2025-07-01 15:01:20 +0100

    Create default.nix

---
+++ default.nix
@@ -0,0 +1,16 @@
+{ nixpkgs }:
+
+nixpkgs.stdenv.mkDerivation {
+  name = "hello-world";
+  version = "1.0";
+
+  unpackPhase = ":";
+  src = null;
+
+  buildPhase = ''
+    mkdir -p $out/bin
+    echo '#!/bin/sh' > $out/bin/hello-world
+    echo 'echo "Hello, World!"' >> $out/bin/hello-world
+    chmod +x $out/bin/hello-world
+  '';
+}
