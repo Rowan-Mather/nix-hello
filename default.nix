b04dafdcf2d67ac82091c09e330a49c69a89eee9
Rowan Mather <76477727+Rowan-Mather@users.noreply.github.com>
2025-11-03 11:49:18 +0000

    Update default.nix

--- default.nix
+++ default.nix
@@ -18,4 +18,21 @@
           chmod +x $out/bin/hello-world
         '';
       };
+
+  failing =
+    let pkgs = import nixpkgs {};
+    in 
+      pkgs.stdenv.mkDerivation {
+        name = "fail-quickly";
+        version = "1.0";
+      
+        unpackPhase = ":";
+        src = null;
+      
+        buildPhase = ''
+          mkdir -p $out/bin
+          exit 1
+        '';
+      };
+
 }
