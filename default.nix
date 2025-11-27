8582d10d02089a96dd77b8a1c0453611152fbc0a
Rowan Mather <76477727+Rowan-Mather@users.noreply.github.com>
2025-11-27 10:05:43 +0000

    Update and rename test.nix to default.nix

--- default.nix
+++ default.nix
@@ -1,7 +1,7 @@
 { nixpkgs }:
 
 {
-  hello =
+  timeout =
     let pkgs = import nixpkgs {};
     in 
       pkgs.stdenv.mkDerivation {
@@ -22,5 +22,24 @@
         meta = {
           timeout = 5;
         };
+      };
+  longbuild =
+    let pkgs = import nixpkgs {};
+    in 
+      pkgs.stdenv.mkDerivation {
+        name = "hello-world";
+        version = "1.0";
+      
+        unpackPhase = ":";
+        src = null;
+      
+        buildPhase = ''
+          mkdir -p $out/bin
+          sleep 60
+          echo '#!/bin/sh' > $out/bin/hello-world
+          echo 'echo "Hello, World"' >> $out/bin/hello-world
+          chmod +x $out/bin/hello-world
+        '';
+
       };
 }
