6e44d719de84c30150c20ab05f7108bfa01bb65f
Rowan Mather <76477727+Rowan-Mather@users.noreply.github.com>
2026-03-11 09:25:53 +0000

    Add meta system feature test

--- default.nix
+++ default.nix
@@ -1,57 +1,72 @@
 { nixpkgs }:
 
+let
+  pkgs = import nixpkgs {} 
+in
 {
   timeout =
-    let pkgs = import nixpkgs {};
-    in 
-      pkgs.stdenv.mkDerivation {
-        name = "hello-world";
-        version = "1.0";
-      
-        unpackPhase = ":";
-        src = null;
-      
-        buildPhase = ''
-          mkdir -p $out/bin
-          sleep 20
-          echo '#!/bin/sh' > $out/bin/hello-world
-          echo 'echo "Hello, World with timer!"' >> $out/bin/hello-world
-          chmod +x $out/bin/hello-world
-        '';
+    pkgs.stdenv.mkDerivation {
+      name = "hello-world";
+      version = "1.0";
+    
+      unpackPhase = ":";
+      src = null;
+    
+      buildPhase = ''
+        mkdir -p $out/bin
+        sleep 20
+        echo '#!/bin/sh' > $out/bin/hello-world
+        echo 'echo "Hello, World with timer!"' >> $out/bin/hello-world
+        chmod +x $out/bin/hello-world
+      '';
 
-        meta = {
-          timeout = 5;
-        };
+      meta = {
+        timeout = 5;
       };
+    };
 
   nulltimeout =
-    let pkgs = import nixpkgs {};
-    in 
-      pkgs.stdenv.mkDerivation {
-        name = "hello-world";
-        version = "1.0";
-      
-        unpackPhase = ":";
-        src = null;
-      
-        buildPhase = ''
-          mkdir -p $out/bin
-          sleep 20
-          echo '#!/bin/sh' > $out/bin/hello-world
-          echo 'echo "Hello, World with timer!"' >> $out/bin/hello-world
-          chmod +x $out/bin/hello-world
-        '';
+    pkgs.stdenv.mkDerivation {
+      name = "hello-world";
+      version = "1.0";
+    
+      unpackPhase = ":";
+      src = null;
+    
+      buildPhase = ''
+        mkdir -p $out/bin
+        sleep 20
+        echo '#!/bin/sh' > $out/bin/hello-world
+        echo 'echo "Hello, World with timer!"' >> $out/bin/hello-world
+        chmod +x $out/bin/hello-world
+      '';
 
-        meta = {
-          timeout = -1;
-        };
+      meta = {
+        timeout = -1;
       };
+    };
 
   hello =
-    let pkgs = import nixpkgs {};
-    in 
+    pkgs.stdenv.mkDerivation {
+      name = "hello-world";
+      version = "1.0";
+    
+      unpackPhase = ":";
+      src = null;
+    
+      buildPhase = ''
+        mkdir -p $out/bin
+        sleep 10
+        echo '#!/bin/sh' > $out/bin/hello-world
+        echo 'echo "Hello, World"' >> $out/bin/hello-world
+        chmod +x $out/bin/hello-world
+      '';
+
+    };
+
+    runlocally = 
       pkgs.stdenv.mkDerivation {
-        name = "hello-world";
+        name = "hello-world-local";
         version = "1.0";
       
         unpackPhase = ":";
@@ -59,11 +74,35 @@
       
         buildPhase = ''
           mkdir -p $out/bin
-          sleep 10
           echo '#!/bin/sh' > $out/bin/hello-world
           echo 'echo "Hello, World"' >> $out/bin/hello-world
           chmod +x $out/bin/hello-world
         '';
 
+        meta = {
+          requiredSystemFeatures = [ "local" ];
+        };
+  
+      };
+
+    runbuilder = 
+      pkgs.stdenv.mkDerivation {
+        name = "hello-world-builder";
+        version = "1.0";
+      
+        unpackPhase = ":";
+        src = null;
+      
+        buildPhase = ''
+          mkdir -p $out/bin
+          echo '#!/bin/sh' > $out/bin/hello-world
+          echo 'echo "Hello, World"' >> $out/bin/hello-world
+          chmod +x $out/bin/hello-world
+        '';
+
+        meta = {
+          requiredSystemFeatures = [ "slan" ];
+        };
+  
       };
 }
